e44601d3c666819a4a045f3b1f5dee28861b696c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-14 19:08:32 +0900

    API

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,11 @@
 
+import binding=require('./binding');
 
-export async function minify(content:string):Promise<string>{
-    // TODO
-    return content
+
+export async function minify(content:Buffer,options:any):Promise<binding.TransformOutput>{
+    return binding.minify(content,toBuffer(options))
 }
+
+function toBuffer(t: any): Buffer {
+    return Buffer.from(JSON.stringify(t))
+  }
