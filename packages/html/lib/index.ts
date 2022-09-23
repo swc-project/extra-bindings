c8e19b9a83ad077fcbbc115d48e51e070dfbba2a
一丝 <yisibl@users.noreply.github.com>
2022-09-23 13:51:46 +0800

    feat: Add `minifySync` and examples (#12)

--- packages/html/lib/index.ts
+++ packages/html/lib/index.ts
@@ -4,6 +4,10 @@ export async function minify(content: Buffer, options: any): Promise<string> {
   return binding.minify(content, toBuffer(options));
 }
 
+export function minifySync(content: Buffer, options: any) {
+  return binding.minifySync(content, toBuffer(options));
+}
+
 function toBuffer(t: any): Buffer {
   return Buffer.from(JSON.stringify(t));
 }
