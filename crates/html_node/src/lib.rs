33ddbb54133fa84774cd2626a802f0038acb3fe4
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-10-06 19:49:20 +0300

    feat(html): Improve API (#16)

--- crates/html_node/src/lib.rs
+++ crates/html_node/src/lib.rs
@@ -10,6 +10,7 @@ use std::{backtrace::Backtrace, env, panic::set_hook};
 use anyhow::{bail, Context};
 use napi::{bindgen_prelude::*, JsString, Task};
 use serde::Deserialize;
+use swc_cached::regex::CachedRegex;
 use swc_common::FileName;
 use swc_html::{
     codegen::{
@@ -18,7 +19,12 @@ use swc_html::{
     },
     parser::parse_file_as_document,
 };
-use swc_html_minifier::minify_document;
+use swc_html_minifier::{
+    minify_document,
+    option::{
+        CollapseWhitespaces, MinifierType, MinifyCssOption, MinifyJsOption, MinifyJsonOption,
+    },
+};
 use swc_nodejs_common::{deserialize_json, get_deserialized, MapErr};
 
 use crate::util::try_with;
@@ -43,6 +49,95 @@ struct MinifyTask {
 pub struct MinifyOptions {
     #[serde(default)]
     filename: Option<String>,
+
+    // Parser options
+    #[serde(default)]
+    iframe_srcdoc: bool,
+    #[serde(default)]
+    scripting_enabled: bool,
+
+    // Minification options
+    #[serde(default)]
+    force_set_html5_doctype: bool,
+    #[serde(default = "default_collapse_whitespaces")]
+    collapse_whitespaces: CollapseWhitespaces,
+    // Remove safe empty elements with metadata content, i.e. the `script` and `style` element
+    // without content and attributes, `meta` and `link` elements without attributes and etc
+    #[serde(default = "true_by_default")]
+    remove_empty_metadata_elements: bool,
+    #[serde(default = "true_by_default")]
+    remove_comments: bool,
+    #[serde(default = "default_preserve_comments")]
+    preserve_comments: Option<Vec<CachedRegex>>,
+    #[serde(default = "true_by_default")]
+    minify_conditional_comments: bool,
+    #[serde(default = "true_by_default")]
+    remove_empty_attributes: bool,
+    #[serde(default = "true_by_default")]
+    remove_redundant_attributes: bool,
+    #[serde(default = "true_by_default")]
+    collapse_boolean_attributes: bool,
+    #[serde(default = "true_by_default")]
+    normalize_attributes: bool,
+    #[serde(default = "minify_json_by_default")]
+    minify_json: MinifyJsonOption,
+    #[serde(default = "minify_js_by_default")]
+    minify_js: MinifyJsOption,
+    #[serde(default = "minify_css_by_default")]
+    minify_css: MinifyCssOption,
+    #[serde(default)]
+    minify_additional_scripts_content: Option<Vec<(CachedRegex, MinifierType)>>,
+    #[serde(default)]
+    minify_additional_attributes: Option<Vec<(CachedRegex, MinifierType)>>,
+    #[serde(default = "true_by_default")]
+    sort_space_separated_attribute_values: bool,
+    #[serde(default)]
+    sort_attributes: bool,
+
+    // Codegen options
+    #[serde(default)]
+    tag_omission: Option<bool>,
+    #[serde(default)]
+    self_closing_void_elements: Option<bool>,
+    #[serde(default)]
+    quotes: Option<bool>,
+}
+
+const fn true_by_default() -> bool {
+    true
+}
+
+const fn minify_json_by_default() -> MinifyJsonOption {
+    MinifyJsonOption::Bool(true)
+}
+
+const fn minify_js_by_default() -> MinifyJsOption {
+    MinifyJsOption::Bool(true)
+}
+
+const fn minify_css_by_default() -> MinifyCssOption {
+    MinifyCssOption::Bool(true)
+}
+
+fn default_preserve_comments() -> Option<Vec<CachedRegex>> {
+    Some(vec![
+        // License comments
+        CachedRegex::new("@preserve").unwrap(),
+        CachedRegex::new("@copyright").unwrap(),
+        CachedRegex::new("@lic").unwrap(),
+        CachedRegex::new("@cc_on").unwrap(),
+        // Allow to keep custom comments
+        CachedRegex::new("^!").unwrap(),
+        // Server-side comments
+        CachedRegex::new("^\\s*#").unwrap(),
+        // Conditional IE comments
+        CachedRegex::new("^\\[if\\s[^\\]+]").unwrap(),
+        CachedRegex::new("\\[endif]").unwrap(),
+    ])
+}
+
+const fn default_collapse_whitespaces() -> CollapseWhitespaces {
+    CollapseWhitespaces::OnlyMetadata
 }
 
 #[napi]
@@ -59,7 +154,7 @@ impl Task for MinifyTask {
     }
 
     fn resolve(&mut self, env: napi::Env, output: Self::Output) -> napi::Result<Self::JsValue> {
-        Ok(env.create_string(&output)?)
+        env.create_string(&output)
     }
 }
 
@@ -72,11 +167,13 @@ fn minify_inner(code: &str, opts: MinifyOptions) -> anyhow::Result<String> {
 
         let fm = cm.new_source_file(filename, code.into());
 
+        let scripting_enabled = opts.scripting_enabled;
         let mut errors = vec![];
         let doc = parse_file_as_document(
             &fm,
             swc_html::parser::parser::ParserConfig {
-                ..Default::default()
+                scripting_enabled,
+                iframe_srcdoc: opts.iframe_srcdoc,
             },
             &mut errors,
         );
@@ -98,10 +195,31 @@ fn minify_inner(code: &str, opts: MinifyOptions) -> anyhow::Result<String> {
             for err in errors {
                 err.to_diagnostics(handler).emit();
             }
+
             bail!("failed to parse input as stylesheet (recovered)")
         }
 
-        minify_document(&mut doc, &Default::default());
+        let options = swc_html_minifier::option::MinifyOptions {
+            force_set_html5_doctype: opts.force_set_html5_doctype,
+            collapse_whitespaces: opts.collapse_whitespaces,
+            remove_empty_metadata_elements: opts.remove_empty_metadata_elements,
+            remove_comments: opts.remove_comments,
+            preserve_comments: opts.preserve_comments,
+            minify_conditional_comments: opts.minify_conditional_comments,
+            remove_empty_attributes: opts.remove_empty_attributes,
+            remove_redundant_attributes: opts.remove_redundant_attributes,
+            collapse_boolean_attributes: opts.collapse_boolean_attributes,
+            normalize_attributes: opts.normalize_attributes,
+            minify_json: opts.minify_json,
+            minify_js: opts.minify_js,
+            minify_css: opts.minify_css,
+            minify_additional_scripts_content: opts.minify_additional_scripts_content,
+            minify_additional_attributes: opts.minify_additional_attributes,
+            sort_space_separated_attribute_values: opts.sort_space_separated_attribute_values,
+            sort_attributes: opts.sort_attributes,
+        };
+
+        minify_document(&mut doc, &options);
 
         let code = {
             let mut buf = String::new();
@@ -117,6 +235,10 @@ fn minify_inner(code: &str, opts: MinifyOptions) -> anyhow::Result<String> {
                     &mut wr,
                     CodegenConfig {
                         minify: true,
+                        scripting_enabled,
+                        tag_omission: opts.tag_omission,
+                        self_closing_void_elements: opts.self_closing_void_elements,
+                        quotes: opts.quotes,
                         ..Default::default()
                     },
                 );
--- packages/html/index.ts
+++ packages/html/index.ts
@@ -1,10 +1,48 @@
 import * as binding from "./binding";
 
-export async function minify(content: Buffer, options?: any): Promise<string> {
+type MinifierType = "js-module" | "js-script" | "json" | "css" | "html";
+
+type Options = {
+  filename?: string;
+  iframeSrcdoc?: boolean;
+  scriptingEnabled?: boolean;
+  forceSetHtml5Doctype?: boolean;
+  collapseWhitespaces?:
+    | "none"
+    | "all"
+    | "smart"
+    | "conservative"
+    | "advanced-conservative"
+    | "only-metadata";
+  removeEmptyMetadataElements?: boolean;
+  removeComments?: boolean;
+  preserveComments: string[];
+  minifyConditionalComments?: boolean;
+  removeEmptyAttributes?: boolean;
+  removeRedundantAttributes?: boolean;
+  collapseBooleanAttributes?: boolean;
+  normalizeAttributes?: boolean;
+  minifyJson?: boolean | { pretty?: boolean };
+  // TODO improve me after typing `@swc/css`
+  minifyJs?: boolean | { parser?: any; minifier?: any; codegen?: any };
+  minifyCss?: boolean | { parser?: any; minifier?: any; codegen?: any };
+  minifyAdditionalScriptsContent?: [string, MinifierType][];
+  minifyAdditionalAttributes?: [string, MinifierType][];
+  sortSpaceSeparatedAttributeValues?: boolean;
+  sortAttributes?: boolean;
+  tagOmission?: boolean;
+  selfClosingVoidElements?: boolean;
+  quotes?: boolean;
+};
+
+export async function minify(
+  content: Buffer,
+  options?: Options
+): Promise<string> {
   return binding.minify(content, toBuffer(options ?? {}));
 }
 
-export function minifySync(content: Buffer, options?: any) {
+export function minifySync(content: Buffer, options?: Options) {
   return binding.minifySync(content, toBuffer(options ?? {}));
 }
 
