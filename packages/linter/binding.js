e17dcc7b2c2157bcdd76560136b9ed4159702f6e
Mathis Møller <thisen-dk@hotmail.com>
2023-01-26 21:18:18 +0100

    Migrate repo to pnpm

--- packages/linter/binding.js
+++ packages/linter/binding.js
@@ -26,13 +26,13 @@ switch (platform) {
     switch (arch) {
       case "arm64":
         localFileExisted = existsSync(
-          join(__dirname, "css.android-arm64.node")
+          join(__dirname, "lint.android-arm64.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.android-arm64.node");
+            nativeBinding = require("./lint.android-arm64.node");
           } else {
-            nativeBinding = require("@swc/lint-android-arm64");
+            nativeBinding = require("@swc/linter-android-arm64");
           }
         } catch (e) {
           loadError = e;
@@ -40,13 +40,13 @@ switch (platform) {
         break;
       case "arm":
         localFileExisted = existsSync(
-          join(__dirname, "css.android-arm-eabi.node")
+          join(__dirname, "lint.android-arm-eabi.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.android-arm-eabi.node");
+            nativeBinding = require("./lint.android-arm-eabi.node");
           } else {
-            nativeBinding = require("@swc/lint-android-arm-eabi");
+            nativeBinding = require("@swc/linter-android-arm-eabi");
           }
         } catch (e) {
           loadError = e;
@@ -60,13 +60,13 @@ switch (platform) {
     switch (arch) {
       case "x64":
         localFileExisted = existsSync(
-          join(__dirname, "css.win32-x64-msvc.node")
+          join(__dirname, "lint.win32-x64-msvc.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.win32-x64-msvc.node");
+            nativeBinding = require("./lint.win32-x64-msvc.node");
           } else {
-            nativeBinding = require("@swc/lint-win32-x64-msvc");
+            nativeBinding = require("@swc/linter-win32-x64-msvc");
           }
         } catch (e) {
           loadError = e;
@@ -74,13 +74,13 @@ switch (platform) {
         break;
       case "ia32":
         localFileExisted = existsSync(
-          join(__dirname, "css.win32-ia32-msvc.node")
+          join(__dirname, "lint.win32-ia32-msvc.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.win32-ia32-msvc.node");
+            nativeBinding = require("./lint.win32-ia32-msvc.node");
           } else {
-            nativeBinding = require("@swc/lint-win32-ia32-msvc");
+            nativeBinding = require("@swc/linter-win32-ia32-msvc");
           }
         } catch (e) {
           loadError = e;
@@ -88,13 +88,13 @@ switch (platform) {
         break;
       case "arm64":
         localFileExisted = existsSync(
-          join(__dirname, "css.win32-arm64-msvc.node")
+          join(__dirname, "lint.win32-arm64-msvc.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.win32-arm64-msvc.node");
+            nativeBinding = require("./lint.win32-arm64-msvc.node");
           } else {
-            nativeBinding = require("@swc/lint-win32-arm64-msvc");
+            nativeBinding = require("@swc/linter-win32-arm64-msvc");
           }
         } catch (e) {
           loadError = e;
@@ -107,24 +107,26 @@ switch (platform) {
   case "darwin":
     switch (arch) {
       case "x64":
-        localFileExisted = existsSync(join(__dirname, "css.darwin-x64.node"));
+        localFileExisted = existsSync(join(__dirname, "lint.darwin-x64.node"));
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.darwin-x64.node");
+            nativeBinding = require("./lint.darwin-x64.node");
           } else {
-            nativeBinding = require("@swc/lint-darwin-x64");
+            nativeBinding = require("@swc/linter-darwin-x64");
           }
         } catch (e) {
           loadError = e;
         }
         break;
       case "arm64":
-        localFileExisted = existsSync(join(__dirname, "css.darwin-arm64.node"));
+        localFileExisted = existsSync(
+          join(__dirname, "lint.darwin-arm64.node")
+        );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.darwin-arm64.node");
+            nativeBinding = require("./lint.darwin-arm64.node");
           } else {
-            nativeBinding = require("@swc/lint-darwin-arm64");
+            nativeBinding = require("@swc/linter-darwin-arm64");
           }
         } catch (e) {
           loadError = e;
@@ -138,12 +140,12 @@ switch (platform) {
     if (arch !== "x64") {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`);
     }
-    localFileExisted = existsSync(join(__dirname, "css.freebsd-x64.node"));
+    localFileExisted = existsSync(join(__dirname, "lint.freebsd-x64.node"));
     try {
       if (localFileExisted) {
-        nativeBinding = require("./css.freebsd-x64.node");
+        nativeBinding = require("./lint.freebsd-x64.node");
       } else {
-        nativeBinding = require("@swc/lint-freebsd-x64");
+        nativeBinding = require("@swc/linter-freebsd-x64");
       }
     } catch (e) {
       loadError = e;
@@ -154,26 +156,26 @@ switch (platform) {
       case "x64":
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "css.linux-x64-musl.node")
+            join(__dirname, "lint.linux-x64-musl.node")
           );
           try {
             if (localFileExisted) {
-              nativeBinding = require("./css.linux-x64-musl.node");
+              nativeBinding = require("./lint.linux-x64-musl.node");
             } else {
-              nativeBinding = require("@swc/lint-linux-x64-musl");
+              nativeBinding = require("@swc/linter-linux-x64-musl");
             }
           } catch (e) {
             loadError = e;
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "css.linux-x64-gnu.node")
+            join(__dirname, "lint.linux-x64-gnu.node")
           );
           try {
             if (localFileExisted) {
-              nativeBinding = require("./css.linux-x64-gnu.node");
+              nativeBinding = require("./lint.linux-x64-gnu.node");
             } else {
-              nativeBinding = require("@swc/lint-linux-x64-gnu");
+              nativeBinding = require("@swc/linter-linux-x64-gnu");
             }
           } catch (e) {
             loadError = e;
@@ -183,26 +185,26 @@ switch (platform) {
       case "arm64":
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "css.linux-arm64-musl.node")
+            join(__dirname, "lint.linux-arm64-musl.node")
           );
           try {
             if (localFileExisted) {
-              nativeBinding = require("./css.linux-arm64-musl.node");
+              nativeBinding = require("./lint.linux-arm64-musl.node");
             } else {
-              nativeBinding = require("@swc/lint-linux-arm64-musl");
+              nativeBinding = require("@swc/linter-linux-arm64-musl");
             }
           } catch (e) {
             loadError = e;
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "css.linux-arm64-gnu.node")
+            join(__dirname, "lint.linux-arm64-gnu.node")
           );
           try {
             if (localFileExisted) {
-              nativeBinding = require("./css.linux-arm64-gnu.node");
+              nativeBinding = require("./lint.linux-arm64-gnu.node");
             } else {
-              nativeBinding = require("@swc/lint-linux-arm64-gnu");
+              nativeBinding = require("@swc/linter-linux-arm64-gnu");
             }
           } catch (e) {
             loadError = e;
@@ -211,13 +213,13 @@ switch (platform) {
         break;
       case "arm":
         localFileExisted = existsSync(
-          join(__dirname, "css.linux-arm-gnueabihf.node")
+          join(__dirname, "lint.linux-arm-gnueabihf.node")
         );
         try {
           if (localFileExisted) {
-            nativeBinding = require("./css.linux-arm-gnueabihf.node");
+            nativeBinding = require("./lint.linux-arm-gnueabihf.node");
           } else {
-            nativeBinding = require("@swc/lint-linux-arm-gnueabihf");
+            nativeBinding = require("@swc/linter-linux-arm-gnueabihf");
           }
         } catch (e) {
           loadError = e;
