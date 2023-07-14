0fe2d1d0554b9f7c4f02f2738041b4c7be61c5f3
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-14 14:42:03 +0900

    typing

--- packages/css/index.ts
+++ packages/css/index.ts
@@ -1,19 +1,44 @@
 import * as binding from "./binding";
 
-export type Options = {
+export type MinifyOptions = {
   filename?: string;
   sourceMap?: boolean;
 };
 
+export type TransformOptions = {
+  filename?: string;
+
+  sourceMap?: boolean
+
+  cssModules?: CssModuleTransformOptions
+
+  minify?: boolean
+}
+
+export type CssModuleTransformOptions = {
+  pattern: String,
+}
+
 export async function minify(
   content: Buffer,
-  options: Options
+  options: MinifyOptions
 ): Promise<binding.TransformOutput> {
   return binding.minify(content, toBuffer(options ?? {}));
 }
 
-export function minifySync(content: Buffer, options: Options) {
+export function minifySync(content: Buffer, options: MinifyOptions) {
   return binding.minifySync(content, toBuffer(options ?? {}));
+}
+
+export async function transform(
+  content: Buffer,
+  options: TransformOptions
+): Promise<binding.TransformOutput> {
+  return binding.transform(content, toBuffer(options ?? {}));
+}
+
+export function transformSync(content: Buffer, options: TransformOptions) {
+  return binding.transformSync(content, toBuffer(options ?? {}));
 }
 
 function toBuffer(t: any): Buffer {
