82093ba8f2ee454e5b793979cf9fe45d7f3a4655
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-19 13:14:12 +0900

    hmm

--- crates/css_node/src/deps.rs
+++ crates/css_node/src/deps.rs
@@ -1,4 +1,6 @@
 use serde::Serialize;
+use swc_css_ast::{ImportHref, ImportPrelude, Url};
+use swc_css_visit::{Visit, VisitWith};
 
 pub struct Analyzer {
     pub deps: Dependencies,
@@ -11,7 +13,28 @@ pub struct Dependencies {
 }
 
 #[derive(Debug, Serialize)]
-pub struct Import {}
+pub struct Import {
+    pub url: CssUrl,
+}
 
 #[derive(Debug, Serialize)]
 pub struct CssUrl {}
+
+impl Visit for Analyzer {
+    fn visit_import_prelude(&mut self, n: &ImportPrelude) {
+        n.layer_name.visit_with(self);
+        n.import_conditions.visit_with(self);
+
+        self.deps.imports.push(Import {
+            url: normalize_import_href(&n.href),
+        });
+    }
+
+    fn visit_url(&mut self, n: &Url) {
+        self.deps.urls.push(normalize_url(n));
+    }
+}
+
+fn normalize_import_href(n: &ImportHref) -> CssUrl {}
+
+fn normalize_url(n: &Url) -> CssUrl {}
